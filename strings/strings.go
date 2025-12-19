9c2fee24334bcf383836b1305fc3dac38d1cf3e1
Lucas127128 <kayauleung2024@gmail.com>
2025-12-19 08:01:14 +0000

    Finish the structs lesson

--- strings/strings.go
+++ strings/strings.go
@@ -4,33 +4,34 @@ import (
 	"fmt"
 	"unicode/utf8"
 )
-func main(){
+
+func main() {
 	const s = "hello"
 	fmt.Println(len(s))
 
-	for i:= range len(s){
+	for i := range len(s) {
 		fmt.Printf("%x", s[i])
 	}
 	fmt.Println("")
 
 	fmt.Println(utf8.RuneCountInString(s))
 
-	for idx, runeValue:=range s{
+	for idx, runeValue := range s {
 		fmt.Printf("%#U starts at %d\n", runeValue, idx)
 		fmt.Println("")
 	}
 
-	for i, w:=0, 0;i<len(s);i+=w{
-		runeValue, width:=utf8.DecodeRuneInString(s[i:])
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeRuneInString(s[i:])
 		fmt.Printf("%#U starts at %d\n", runeValue, i)
-		w=width
+		w = width
 		examineResult(runeValue)
 	}
 }
-	func examineResult(r rune) {
-		if r=='h' {
-			fmt.Println("found h")
-		} else if r=='e'{
-			fmt.Println("Found e")
-		}
+func examineResult(r rune) {
+	if r == 'h' {
+		fmt.Println("found h")
+	} else if r == 'e' {
+		fmt.Println("Found e")
 	}
+}
